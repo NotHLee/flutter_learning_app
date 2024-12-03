611c344a8f0f42ae2fddb3c6eab33e261d074e9e
Hao Yang Lee <144778275+NotHLee@users.noreply.github.com>
2024-12-03 15:01:57 +0800

    Added arrow back and dots svg

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,17 +7,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      // app bar
       appBar: AppBar(
         title: Text(
           'Breakfast',
           style: TextStyle(
             fontWeight: FontWeight.bold,
-          ),
+          )
         ),
         centerTitle: true,
-      ),
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/arrow_back.svg',
+            height: 20,
+            width: 20,
+          )
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 20,
+              width: 20,
+            )
+          )
+        ],
+      )
     );
   }
 }
