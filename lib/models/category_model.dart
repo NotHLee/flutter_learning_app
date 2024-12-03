59a6a770b5a32883c06561a611005628689f67de
Hao Yang Lee <144778275+NotHLee@users.noreply.github.com>
2024-12-03 17:31:04 +0800

    Added data model for categorical objects to be displayed in boxes

---
+++ lib/models/category_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  final String name;
+  final String iconPath;
+  final Color boxColor;
+
+  CategoryModel({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+  });
+}
